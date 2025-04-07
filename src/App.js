40de9b1e584413b85e1f87d6b40dbd2f33d9133a
Everton Ferreira Lima <Evertonlima0406@hotmail.com>
2025-04-07 18:05:26 -0300

    Links do react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/SobreMim";
+import Menu from "./componentes/Menu";
 
 
 function App() {
   return (
     <BrowserRouter>
+      <Menu />
       <Routes>  
         <Route path="/" element={<Inicio />} />
         <Route path="/sobremim" element={<SobreMim />} />
