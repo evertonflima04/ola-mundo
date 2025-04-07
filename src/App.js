03c68fb517f65e851f9c63c18b3e7b47f3c1a8d1
Everton Ferreira Lima <Evertonlima0406@hotmail.com>
2025-04-07 17:47:20 -0300

    Criando roteador

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/SobreMim";
 
 
 function App() {
   return (
     <BrowserRouter>
-      <Inicio />
+      <Routes>  
+        <Route path="/" element={<Inicio />} />
+        <Route path="/sobremim" element={<SobreMim />} />
+        <Route path="*" element={<h1>Página não encontrada</h1>} />        
+      </Routes>
       
     </BrowserRouter>
   );
