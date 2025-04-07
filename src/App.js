59e31ada9328967b79e3d0d43509f1b923f47d3b
Everton Ferreira Lima <Evertonlima0406@hotmail.com>
2025-04-07 17:18:26 -0300

    Alternando entre rotas com JS

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import Inicio from "./paginas/inicio";
+import SobreMim from "./paginas/SobreMim";
+
+console.log(window.location);
+
+const pagina = window.location.pathname === "/" ? <Inicio /> : <SobreMim />;
 
 function App() {
-  return (
-    <h1>Olá Mundo</h1>
-  );
+  return pagina;
 }
 
 export default App;
