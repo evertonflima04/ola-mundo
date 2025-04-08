d9823ec492a2b23d93834e0e13b94d835be5d76d
Everton Ferreira Lima <Evertonlima0406@hotmail.com>
2025-04-07 21:27:42 -0300

    Desenvolvendo inicio

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/inicio";
 import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
+import Rodape from "componentes/Rodape";
 
 
 function AppRoutes() {
@@ -13,6 +14,7 @@ function AppRoutes() {
         <Route path="/sobremim" element={<SobreMim />} />
         <Route path="*" element={<h1>Página não encontrada</h1>} />        
       </Routes>
+      <Rodape />
       
     </BrowserRouter>
   );
